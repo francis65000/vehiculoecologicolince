16d3891ee79c46a6b594245c3eeef7d241219fa4
franklin3499@gmail.com <145580354+francis65000@users.noreply.github.com>
2024-03-20 15:32:20 +0100

    botones de cerrar sesion en front

--- resources/views/comunes/menu.blade.php
+++ resources/views/comunes/menu.blade.php
@@ -11,31 +11,50 @@
         <ul class="navbar-nav mr-auto menuPrincipal">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown1" role="button"
-                   data-bs-toggle="dropdown" aria-expanded="false">
+                    data-bs-toggle="dropdown" aria-expanded="false">
                     Conócenos
                 </a>
                 <ul class="dropdown-menu submenuInicio" aria-labelledby="navbarDropdown1">
-                    <li><a class="dropdown-item text-white" href="{{url('/conocenos/sobre-nosotros')}}">Sobre nosotros</a></li>
-                    <li><a class="dropdown-item text-white" href="{{url('/conocenos/equipos')}}">Equipos</a></li>
-                    <li><a class="dropdown-item text-white" href="{{url('/conocenos/pilotos')}}">Pilotos</a></li>
-                    <li><a class="dropdown-item text-white" href="{{url('/conocenos/dorsales')}}">Dorsales</a></li>
+                    <li><a class="dropdown-item text-white" href="{{ url('/conocenos/sobre-nosotros') }}">Sobre
+                            nosotros</a></li>
+                    <li><a class="dropdown-item text-white" href="{{ url('/conocenos/equipos') }}">Equipos</a></li>
+                    <li><a class="dropdown-item text-white" href="{{ url('/conocenos/pilotos') }}">Pilotos</a></li>
+                    <li><a class="dropdown-item text-white" href="{{ url('/conocenos/dorsales') }}">Dorsales</a></li>
                 </ul>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white" href="{{url('vehiculos')}}">Vehículos</a>
+                <a class="nav-link text-white" href="{{ url('vehiculos') }}">Vehículos</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white" href="{{url('blog')}}">Blog</a>
+                <a class="nav-link text-white" href="{{ url('blog') }}">Blog</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white" href="{{url('reconocimientos')}}">Reconocimientos</a>
+                <a class="nav-link text-white" href="{{ url('reconocimientos') }}">Reconocimientos</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white" href="{{url('contacto')}}">Contacto</a>
+                <a class="nav-link text-white" href="{{ url('contacto') }}">Contacto</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white" href="https://lince-jandula.blogspot.com/" target="_blank">Web Antigua</a>
+                <a class="nav-link text-white" href="https://lince-jandula.blogspot.com/" target="_blank">Web
+                    Antigua</a>
             </li>
+            @if (Auth::check())
+                <li class="nav-item">
+                    <a href="{{ url('/escritorio') }}" class="btn btn-primary">
+                        <i class="menu-icon fa-solid fa-table-columns"></i>
+                        <div data-i18n="Dashboards">Escritorio</div>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="btn btn-danger">
+                            <i class="menu-icon fa-solid fa-right-from-bracket"></i>
+                            Cerrar sesión
+                        </button>
+                    </form>
+                </li>
+            @endif
         </ul>
         <ul class="navbar-nav ml-auto menuRedesSociales">
             <li class="nav-item">
@@ -53,4 +72,3 @@
         </ul>
     </div>
 </nav>
-
